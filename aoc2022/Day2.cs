18d66171de3575c34f8d8868c62699ee275411f6
bengto <bengto@axis.com>
2022-11-23 12:56:46 +0100

    Initial commit

---
+++ aoc2022/Day2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2022
+{
+    internal class Day2
+    {
+        public void Part1()
+        {
+            var data = File.ReadAllLines(@"data\day2.txt");
+
+            var values = data.Select(r => r.Select(c => Int32.Parse(c.ToString())).ToArray()).ToArray();
+
+            Console.WriteLine($"Answer is {42}");
+        }
+
+        public void Part2()
+        {
+            var data = File.ReadAllLines(@"data\day2.txt");
+
+            var values = data.Select(r => r.Select(c => Int32.Parse(c.ToString())).ToArray()).ToArray();
+
+            Console.WriteLine($"Answer is {42}");
+        }
+
+    }
+}
---
+++ aoc2022/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2022
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Day1().Part1();
+        }
+    }
+}
